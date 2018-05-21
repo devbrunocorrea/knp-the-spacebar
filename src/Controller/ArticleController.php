e76b2eb07bc488b51794ba02fabb777dd76e1cb2
Bruno W <bruno.correa.at@gmail.com>
2018-05-20 21:52:30 -0300

    finish project

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+//use Twig\Environment;
 
 class ArticleController extends AbstractController {
 
@@ -30,7 +33,22 @@ class ArticleController extends AbstractController {
 
     return $this->render('article/show.html.twig',[
       'title' => ucwords(str_replace('-',' ', $slug)),
+      'slug' => $slug,
       'comments' => $comments,
     ]);
   }
+
+  /**
+  * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+  */
+  public function toggleArticleHeart($slug, LoggerInterface $logger){
+    //@ TODO - actually hear/unheart the article!
+
+    //return new Response(json_encode('hearts' => 5));
+    //return $this->json(['hearts' => rand(5,100)]);
+
+    $logger->info('Article is being hearted');
+    return new JsonResponse(['hearts' => rand(5,100)]);
+  }
+
 }
